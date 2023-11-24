9c04293caf24215591a576a70d30467aaa92a32f
chris017 <chrisi.schmid112@gmail.com>
2023-11-24 08:24:51 +0100

    added Frontend Code

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Ap">
+      <h1 className="text-3xl font-bold text-cyan-300 text-center pt-20">
+      Frontend Test!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
