e7ac86ddd455fe3eca9fcdbc7f00b73c76e17d4f
nklsdhbw <nklsdhbw@gmx.de>
2024-01-23 20:48:15 +0100

    display labels in percentage in bubble chart

--- frontend/src/pages/charts/Fdp.js
+++ frontend/src/pages/charts/Fdp.js
@@ -234,7 +234,9 @@ const Fdp = () => {
         type: "bubble",
         data: bubbleData,
         options: {devicePixelRatio:6,
-          scales: {y: {title: {display: true, text: "Negative Sentiment"}}, x: {title: {display: true, text: "Positive Sentiment"}}},
+          scales: {y: {title: {display: true, text: "Negative Sentiment"}, ticks: {callback: function(value) {
+            return value.toFixed(1) + '%'}}}, x: {title: {display: true, text: "Positive Sentiment"},ticks: {callback: function(value) {
+              return value.toFixed(1) + '%'}}}},
           responsive: true,
           plugins: {
             legend: {
@@ -244,8 +246,7 @@ const Fdp = () => {
             title: {
               display: true,
               text: "Verhältnis von positiven und negativen Sätzen je Partei",
-            },
-            
+            },  
           }
         },
       };
