34166558977b18a3a87840f4c65808ea18e4edaa
nklsdhbw <nklsdhbw@gmx.de>
2024-01-23 21:02:26 +0100

    removed learn more button and activated dashboard buton

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -14,13 +14,10 @@ export default function Hero(){
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="/dashboard"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Dashboard
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
